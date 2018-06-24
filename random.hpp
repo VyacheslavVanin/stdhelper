c5bc7ed1c0af03bf52c11d79a4b8f4641a9ad526
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2018-06-24 16:00:53 +0300

    Add random

---
+++ random.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <random>
+
+namespace vvv {
+
+template <typename T, typename = std::enable_if_t<std::is_scalar<T>::value>>
+inline T random(T min, T max)
+{
+    static std::random_device r;
+    static std::default_random_engine e1(r());
+    std::uniform_int_distribution<T> uniform_dist(min, max);
+    return uniform_dist(e1);
+}
+
+template <typename T,
+          typename = std::enable_if_t<std::is_floating_point<T>::value>>
+inline T random_real(T min, T max)
+{
+    static std::random_device r;
+    static std::default_random_engine e1(r());
+    std::uniform_real_distribution<T> uniform_dist(min, max);
+    return uniform_dist(e1);
+}
+
+template <>
+inline float random<float>(float min, float max)
+{
+    return random_real<float>(min, max);
+}
+template <>
+inline double random<double>(double min, double max)
+{
+    return random_real<double>(min, max);
+}
+} // namespace vvv
